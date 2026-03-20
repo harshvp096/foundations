0c30213d70cab6a5155d90cea1b5bd5154101d89
harshvp096 <harshvp096@gmail.com>
2026-03-20 22:32:15 +0530

    docs: initial logic and ASCII notes

---
+++ Basics/Basics1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"ASCII values -";
+//a,b,c..... = 97 - 122
+//A,B,C..... = 65 - 90
+//0,1,2..... = 48 - 57
+
+cout<<"solving expression";
+//a=7/22*(3.14+2)*3/5 = ?
+//first solve brackets 
+//7/22*5.14*3/5
+//use bodmas and if same priority then solve from left (for these 4)
+//0*5.14*3/5
+//solve 0 and 5.14 first (= 0)
+//0*3/5 
+//solve 0 and 3 first (= 0)
+//now 0/5 which is also 0
+}
