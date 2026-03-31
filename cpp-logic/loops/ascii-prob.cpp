0ca9314f349617a5acab925d8dda0ba0234e55f0
harshvp096 <harshvp096@gmail.com>
2026-03-31 18:38:58 +0530

    advanced-loop-problems

---
+++ cpp-logic/loops/ascii-prob.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     int x;
+//     cout<<"Enter the number : ";
+//     cin>>x;
+//     int sum = 0;
+//     char ch;
+//     for(int i=1;i<=x;i++){
+//         sum = i;
+//         cout<<"ASCII of "<<i<<" is "<<(char)sum<<endl;
+// }
+
+//---------------------------------------------
+
+int x;
+for(int i=65;i<=90;i++){
+    cout<<"ASCII value of "<<i<<" is : "<<(char)i<<endl;
+}
+return 0;
+}
