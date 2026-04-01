d02f2f04547355cd472af2ac0a4c81776c3403aa
harshvp096 <harshvp096@gmail.com>
2026-04-01 23:03:34 +0530

    details-about-loops

---
+++ cpp-logic/loops/loopbrackets.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=10;i++)
+    cout<<i<<" ";
+//this will work without curly brackets cause only 1 condtn.
+}
+
+//if more condition then use of curly needed or else upper codes 
+//will run and then second wil get a chance so might seem like this -
+// 1234567891010 so like this 
+// if used with {} - 1 10 2 10 3 10 4 10 etc!
